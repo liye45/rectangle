3063179cd4a2ac5dca19b53bbdd751fc4175c4b9
liye45 <1412154147@qq.com>
2019-03-27 15:00:19 +0800

    v0.5

--- rectangle.js
+++ rectangle.js
@@ -1,11 +1,26 @@
-
 $(function() {
     var $width = $('#width'),
         $height = $('#height'),
         $btnCal = $('#calculate'),
+        $widthValidation=$('#$widthValidation')
+        $heightValidation=$('#$heightValidation')
         $perimeter = $('#perimeter'),
         $area = $('#area');
-
+    
+$width.focusout(function(){
+  var w=$width.val();
+  if(w===''){
+    $widthValidation.html('宽度不能为空！');
+    $width.select();
+  }
+})
+$height.focusout(function(){
+  var h=$height.val();
+  if(h===''){
+    $heightValidation.html('高度不能为空！');
+    $height.select();
+  }
+})
     $btnCal.click(function(){
           var w = $width.val(),
               h = $height.val();
