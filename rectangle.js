870ada9d1dc1a5b301a73158a3e05822783f6e4c
liye45 <1412154147@qq.com>
2019-04-17 16:38:39 +0800

    v1.1

--- rectangle.js
+++ rectangle.js
@@ -1,20 +1,68 @@
-
 $(function() {
     var $width = $('#width'),
         $height = $('#height'),
         $btnCal = $('#calculate'),
         $perimeter = $('#perimeter'),
-        $area = $('#area');
+        $area = $('#area'),
+        $widthValidate = $('#width-validate'),
+        $heightValidate = $('#height-validate'),
+        isPassValidate = false;
 
-    $btnCal.click(function(){
+$width.focusout(function() {
+      var result = validate($width.val());
+          isPassValidate = result.isOK;
+          if(!result.isOK) {
+                  $widthValidate.html('宽度' + result.reason);
+                  $width.select();
+                            
+          } else {
+                  $widthValidate.html('');
+                      
+          }
+            
+});
+
+$width.keypress(function(e) {
+  if(!isLegalKey(e.key, e.target.value, e.target.selectionStart)) {
+          e.preventDefault();
+              
+  }
+    
+});
+
+$height.focusout(function() {
+      var result = validate($height.val());
+          isPassValidate = result.isOK;
+          if(!result.isOK) {
+                  $heightValidate.html('高度' + result.reason);
+                  $height.select();
+                            
+          } else {
+                  $heightValidate.html('');
+                      
+          }
+            
+});
+
+$height.keypress(function(e) {
+  if(!isLegalKey(e.key, e.target.value, e.target.selectionStart)) {
+          e.preventDefault();
+              
+  }
+    
+});
+
+$btnCal.click(function(){
+      if(!isPassValidate) return;
+
           var w = $width.val(),
-              h = $height.val();
+          h = $height.val();
 
-          var r = new Rectangle(w, h);
+         var r = new Rectangle(w, h);
 
-                $perimeter.val(r.perimeter());
-                $area.val(r.area());
-                  
-    });
+         $perimeter.val(r.perimeter());
+         $area.val(r.area());
+              
+});
 
 });
